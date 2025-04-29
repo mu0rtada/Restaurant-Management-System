b13da50188378728863cc23b3ba33c54f3e22afb
mu0rtada <157833614+mu0rtada@users.noreply.github.com>
2025-04-29 12:33:13 +0300

    change form

--- Restaurant.UI/Form1.cs
+++ Restaurant.UI/Form1.cs
@@ -16,5 +16,10 @@ namespace Restaurant.UI
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Label label = new Label();
+        }
     }
 }
