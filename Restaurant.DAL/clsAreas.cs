b7e617727f6a6a5419f12f6d3f8b83f2de769170
mu0rtada <157833614+mu0rtada@users.noreply.github.com>
2025-04-29 12:07:45 +0300

    First Push Project

--- Restaurant.DAL/clsAreas.cs
+++ Restaurant.DAL/clsAreas.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Restaurant.DAL
 {
     public class clsAreas
     {
-        
+       public static DataTable GetAllAreas()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection("your_connection_string"))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Areas", conn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+      
     }
 }
