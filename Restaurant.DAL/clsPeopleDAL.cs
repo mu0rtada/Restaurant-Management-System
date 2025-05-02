2b9dc40a3b1613378b4be3abe5c45e098cc5e2b2
mu0rtada <157833614+mu0rtada@users.noreply.github.com>
2025-05-02 06:36:31 +0300

    create Class People
    Add class for person

---
+++ Restaurant.DAL/clsPeopleDAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Restaurant.DAL
+{
+    public class clsPeopleDAL
+    {
+        // Get all people from a view
+        public static DataTable GetPeopleActive()
+        {
+            string Query = "select * from View_People order by PersonID";
+            DataTable Table = new DataTable();
+            using (SqlConnection Connection = new SqlConnection(StrConnectionSetting.ConnectionString))
+            {
+                using (SqlCommand Command = new SqlCommand(Query, Connection))
+                {
+                    Connection.Open();
+                    SqlDataReader Reade = Command.ExecuteReader();
+                    Table.Load(Reade); // Load results into DataTable
+                }
+            }
+            return Table;
+        }
+  
+
+       
+    }
+}
